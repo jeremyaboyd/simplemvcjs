c663434fc664724fbaaaac29a2034ce232085f87
root <root@kippy.io>
2020-07-24 00:08:05 +0000

    include simplemvcsmtp in simplemvc.js

--- src/simplemvc.js
+++ src/simplemvc.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 const SimpleMVCApp = require('./simplemvc.app');
 const SimpleMVCController = require('./simplemvc.controller.js');
 const SimpleMVCMembership = require('./simplemvc.membership.js');
+const SimpleMVCSMTP = require('./simplemvc.smtp.js');
 
 module.exports.App = SimpleMVCApp;
 module.exports.Controller = SimpleMVCController;
 module.exports.Membership = SimpleMVCMembership;
+module.exports.SimpleMVCSMTP = SimpleMVCSMTP;
